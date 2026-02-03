bc70f73a7eea9924e629c54ba12e27206fa21ed8
udithnalaka <udithnalaka@yahoo.com.au>
2026-02-04 09:23:27 +1000

    add swagger support

--- src/main/java/com/ud/TaskActivity/TaskController.java
+++ src/main/java/com/ud/TaskActivity/TaskController.java
@@ -1,5 +1,6 @@
 package com.ud.TaskActivity;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +8,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableMethodSecurity
 public class TaskController {
 
+    @Operation(summary = "Get a Task")
     @GetMapping(path="/task")
     @PreAuthorize("hasAuthority('SCOPE_readtask')")
     public String getTask() {
         return "Access Granted: Get a Task";
     }
 
+    @Operation(summary = "Update a Task")
     @PostMapping("/task")
     @PreAuthorize("hasAuthority('SCOPE_updatetask')")
     public String updateTask() {
